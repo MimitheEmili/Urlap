24eba501cdacfaf4f2033e2771102839b53ceae7
MimitheEmili22 <mozsikel@gmail.hu>
2024-09-26 19:28:01 +0200

    3. Js megcsinálva valamilyen szinten
    Még azért próbálni egy jobb version-t csinálni

--- Urlap.js
+++ Urlap.js
@@ -1,6 +1,24 @@
 /*Adatok elmentése*/
 
+function Regisztracioonclick() {
+    const name = document.querySelector('input[name="Nev"]').value;
+    const email = document.querySelector('input[name="Email"]').value;
+    const password = document.getElementById('jelszo').value;
+    const age = document.querySelector('select[name="age"]').value;
+    const gender = document.querySelector('input[name="gender"]:checked') ? 
+                   document.querySelector('input[name="gender"]:checked').value : '';
 
+    const userData = {
+        name: name,
+        email: email,
+        password: password,
+        age: age,
+        gender: gender
+    };
+
+    localStorage.setItem('userData', JSON.stringify(userData));
+    alert('A regisztrációs adatok sikeresen elmentve!');
+}
 
 
 function Passwordonclick() {
@@ -14,4 +32,10 @@ function Passwordonclick() {
     }
 
     document.getElementById('jelszo').value = password; // A generált jelszó beírása a mezőbe
+}
+
+const storedData = localStorage.getItem('userData');
+if (storedData) {
+    const userData = JSON.parse(storedData);
+    console.log(userData); // Az eltárolt adatok kiírása a konzolra
 }
