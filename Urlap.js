87d5055e737a36f4ad708c90c7b1e16fb3cf2efc
MimitheEmili <147054667+MimitheEmili@users.noreply.github.com>
2024-10-01 08:26:33 +0200

    4.new version

--- Urlap.js
+++ Urlap.js
@@ -1,41 +1,46 @@
 /*Adatok elmentése*/
 
 function Regisztracioonclick() {
+    //adatok begyűjtése
     const name = document.querySelector('input[name="Nev"]').value;
     const email = document.querySelector('input[name="Email"]').value;
     const password = document.getElementById('jelszo').value;
+    const profession = document.querySelector('input[name="foglalkozas"]').value;
     const age = document.querySelector('select[name="age"]').value;
     const gender = document.querySelector('input[name="gender"]:checked') ? 
                    document.querySelector('input[name="gender"]:checked').value : '';
 
+                   //adatok objektumba eltárolás
     const userData = {
         name: name,
         email: email,
         password: password,
+        profession: profession,
         age: age,
         gender: gender
     };
-
+    //json formátumban elmentve, alert: sikeres
     localStorage.setItem('userData', JSON.stringify(userData));
     alert('A regisztrációs adatok sikeresen elmentve!');
 }
 
 
+
 function Passwordonclick() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+'; //a karakter lista
     let password = '';
-    const passwordLength = 12; // Állítsd be a kívánt jelszó hosszát
+    const passwordLength = 12; // hosszúság
 
     for (let i = 0; i < passwordLength; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
-        password += characters.charAt(randomIndex);
+        password += characters.charAt(randomIndex); //itt generálja le
     }
 
-    document.getElementById('jelszo').value = password; // A generált jelszó beírása a mezőbe
+    document.getElementById('jelszo').value = password; //elmenti
 }
 
 const storedData = localStorage.getItem('userData');
 if (storedData) {
     const userData = JSON.parse(storedData);
-    console.log(userData); // Az eltárolt adatok kiírása a konzolra
+    console.log(userData); // console.logozza
 }
