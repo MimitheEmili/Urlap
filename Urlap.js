6694aaff127870d77a10b0bf32b19accf7fa2a83
MimitheEmili22 <mozsikel@gmail.hu>
2024-10-06 23:40:17 +0200

    Done.

--- Urlap.js
+++ Urlap.js
@@ -1,7 +1,7 @@
 /*Adatok elmentése*/
 
 function Regisztracioonclick() {
-    //adatok begyűjtése
+    // Adatok begyűjtése
     const name = document.querySelector('input[name="Nev"]').value;
     const email = document.querySelector('input[name="Email"]').value;
     const password = document.getElementById('jelszo').value;
@@ -11,7 +11,13 @@ function Regisztracioonclick() {
     const gender = document.querySelector('input[name="gender"]:checked') ? 
                    document.querySelector('input[name="gender"]:checked').value : '';
 
-                   //adatok objektumba eltárolás
+    // Ellenőrzés, hogy minden mező ki van-e töltve
+    if (!name || !email || !password || !profession || !agazat || !age || !gender) {
+        alert('Kérem töltse ki az összes mezőt!');
+        return;
+    }
+
+    // Adatok objektumba eltárolás
     const userData = {
         name: name,
         email: email,
@@ -21,7 +27,8 @@ function Regisztracioonclick() {
         age: age,
         gender: gender
     };
-    //json formátumban elmentve, alert: sikeres
+
+    // JSON formátumban elmentve, alert: sikeres
     localStorage.setItem('userData', JSON.stringify(userData));
     alert('A regisztrációs adatok sikeresen elmentve!');
 }
@@ -29,18 +36,19 @@ function Regisztracioonclick() {
 /*Jelszó generálás*/ 
 
 function Passwordonclick() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+'; //a karakter lista
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+'; // A karakter lista
     let password = '';
-    const passwordLength = 12; // hosszúság
+    const passwordLength = 12; // Hosszúság
 
     for (let i = 0; i < passwordLength; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
-        password += characters.charAt(randomIndex); //itt generálja le
+        password += characters.charAt(randomIndex); // Itt generálja le
     }
 
-    document.getElementById('jelszo').value = password; //elmenti
+    document.getElementById('jelszo').value = password; // Elmenti a jelszót
 }
 
+// Korábban elmentett adatok betöltése és ellenőrzés
 const storedData = localStorage.getItem('userData');
 if (storedData) {
     const userData = JSON.parse(storedData);
